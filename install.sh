0188c3c863d0633b0e240ca0a8d3b449eb7ecbd7
mjtl <146027796+mjtl04@users.noreply.github.com>
2025-10-23 17:32:10 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash  
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install xorg i3
+sudo apt install blueman bluex bluetooth network-manager curl
+sudo apt install vim kitty
+
+sudo systemctl enable bluetooth
+sudo systemctl start bluetooth
+
+sudo systemctl enable NetworkManager
+sudo systemctl start NetworkManager
+
+
+curl -fsS https://dl.brave.com/install.sh | sh
+
+#update .xinitrc -> exec i3
+#update .bash_profile -> 
+
+        #if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+        #    startx
+        #fi
